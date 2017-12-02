17bd4fec7162f3148905623c37670efb223b4923
Mirandatz <hardly.a.challenge@gmail.com>
2017-12-02 02:03:23 -0200

    Deleted unused file ckdtree
    Created function to compute density reachable points.

--- cdenstream/cdbscan.py
+++ cdenstream/cdbscan.py
@@ -1,26 +1,55 @@
-from sklearn.datasets import load_iris
-from ckdtree import ckdtree
+from itertools import combinations
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.neighbors import KDTree
+from constraint import *
+import numpy as np
+
+NEIGHBORHOOD_MIN_POINTS = 50
+NEIGHBORHOOD_RADIUS = 2
+
+
+def compute_density_reachable_points(dataset, point_index, maximum_distance):
+    element_count = dataset.shape[0]
+    distances = pairwise_distances(X=dataset, metric="euclidean")
+
+    def compute_neighbors(element_index):
+        return tuple(i for i in range(element_count)
+                     if distances[point_index, i] <= maximum_distance)
+
+    # Storing points already visited to prevent infinite loops
+    points_already_visited = set()
+
+    # The initial set of density_reachable points is the neighborhood of
+    # the point
+    density_reachable = set(compute_neighbors(point_index))
+
+    new_points_to_explore = True
+    while new_points_to_explore:
+        old_reachable_point_count = len(density_reachable)
+        reachable_neighborhoods = [compute_neighbors(i) for i in density_reachable
+                                   if i not in points_already_visited]
+        points_already_visited.update(density_reachable)
+        for neighborhood in reachable_neighborhoods:
+            density_reachable.update(neighborhood)
+        new_points_to_explore = old_reachable_point_count < len(density_reachable)
+
+    return density_reachable
 
 
 def run_test():
-    iris = load_iris()
-    X = iris.data
-    y = iris.target
-    print(X.shape)
+    X = [
+        (0, 0),
+        (0, 1),
+        (1, 0),
+        (10, 0),
+        (11, 1),
+        (9, 0)]
+    X = np.array(X)
 
-    must_link = [(0, 1),
-                 (1, 2),
-                 (2, 3),
-                 (3, 4),
-                 (4, 5)]
-
-    cannot_link = [(5, 10),
-                   (13, 12),
-                   (149, 12)]
-
-    must_link, cannot_link = sanitize_and_expand_constraints(X=X, must_link=must_link, cannot_link=cannot_link)
-    print(f"must_link: {must_link}")
-    print(f"cannot_link: {cannot_link}")
+    dr = compute_density_reachable_points(dataset=X,
+                                          point_index=0,
+                                          maximum_distance=NEIGHBORHOOD_RADIUS)
+    print(dr)
 
 
 if __name__ == "__main__":
--- cdenstream/ckdtree.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def ckdtree(X, cannot_link):
-    clusters = []
-    shape = X.shape
-    for d in range(shape[1]):
-        median = np.median(X[:, d])
-        print(median)
