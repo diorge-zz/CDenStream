9aa07909013f9c2302540f78f43ac59d9a2c96e0
Diorge Brognara <diorge.bs@gmail.com>
2017-12-05 15:40:51 -0200

    Basic CDenStream unit test to catch obvious bugs

---
+++ cdenstream/tests/test_cdenstream.py
@@ -0,0 +1,35 @@
+"""Module for C-DenStream unit tests
+"""
+
+
+import numpy as np
+from ..cdbscan import cdbscan
+from ..cdenstream import CDenStream
+
+
+def test_simple_stream():
+    """Basic test for a short stream (1 update),
+    no outliers and no concept drift
+    """
+    train_dataset = np.array([[0, 0],
+                              [10, 11],
+                              [12, 10],
+                              [1, 0],
+                              [2, -1],
+                              [11, 12]])
+    precluster = cdbscan(train_dataset, epsilon=3, minpts=2)
+    points = []
+    for cluster in precluster:
+        clusterpoints = train_dataset[list(cluster)]
+        points.append(clusterpoints)
+
+    denstream = CDenStream(ndim=2, mindist=3, minpts=2, outlier_radius=1)
+    denstream.initialize(points)
+
+    denstream.point_arrival([3, 0], 1)
+    denstream.point_arrival([2, 1], 2)
+    denstream.point_arrival([1, 1], 3)
+    denstream.point_arrival([9, 10], 4)
+    denstream.update(4)
+
+    print(denstream.query(0.5))
