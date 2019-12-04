7250e91060ec525207b54413d069dc5039b0dfa4
Diorge Brognara <diorge.bs@gmail.com>
2019-12-04 18:45:59 -0300

    Setup.py first version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="CDenStream",
+    version="0.1.0",
+    packages=['cdenstream'],
+    install_requires=['scikit-learn'],
+    url='https://github.com/diorge/CDenStream'
+)
